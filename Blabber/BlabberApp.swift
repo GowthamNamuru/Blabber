ebb7acbf92b6cf793771684725bbe151e6df7d6d
gowthamnamuru <gowthamnamuri@gmail.com>
2024-04-18 08:50:59 +0530

    Initial Commit

---
+++ Blabber/BlabberApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlabberApp.swift
+//  Blabber
+//
+//  Created by Gowtham Namuru on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BlabberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blabber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blabber
+//
+//  Created by Gowtham Namuru on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
