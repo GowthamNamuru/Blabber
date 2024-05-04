94f0edf9e8ad327271e2db30c586407b33e05968
gowthamnamuru <gowthamnamuri@gmail.com>
2024-05-04 14:57:42 +0530

    Updated chat view.

--- Blabber/ChatView.swift
+++ Blabber/ChatView.swift
@@ -10,16 +10,88 @@ import SwiftUI
 struct ChatView: View {
     @ObservedObject var model: BlabberModel
 
+    /// The message that the user has typed.
+    @State var message = ""
+
+    @FocusState var isFocused: Bool
+
+    /// The last error message that happened.
+    @State var lastErrorMessage = "" {
+      didSet {
+        isDisplayingError = true
+      }
+    }
+
+    @State var isDisplayingError = false
+
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
         VStack {
             ScrollView(.vertical) {
                 ScrollViewReader { reader in
                     ForEach($model.messages) { message in
-                        
+                        MessageView(message: message, myUser: model.userName)
+                    }
+                    .onChange(of: model.messages.count) { _, _ in
+                        guard let last = model.messages.last else { return }
+
+                        withAnimation(.easeOut) {
+                            reader.scrollTo(last.id, anchor: .bottomTrailing)
+                        }
                     }
                 }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+            HStack {
+                Button {
+                    // To track location
+                } label: {
+                    Image(systemName: "location.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(Color.gray)
+                }
+
+                Button {
+                    // For timer
+                } label: {
+                    Image(systemName: "timer")
+                        .font(.title)
+                        .foregroundStyle(Color.gray)
+                }
+                
+                TextField(text: $message, prompt: Text("Message")) {
+                    Text("Enter Message")
+                }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .focused($isFocused)
+                .onSubmit {
+                    // On submission
+
+                    isFocused = true
+                }
+
+                Button {
+                    // Send action
+                } label: {
+                    Image(systemName: "arrow.up.circle.fill")
+                      .font(.title)
+                }
+
+            }
         }
+        .padding()
+        .onAppear {
+            isFocused = true
+        }
+        .alert(Text("Error"), isPresented: $isDisplayingError, actions: {
+            Button("Close", role: .cancel) {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }, message: {
+            Text(lastErrorMessage)
+        })
     }
 }
 
